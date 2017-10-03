0f35c236aba6e4f710112ad4f3d87a6de2af8351
Alexander Falk <alexanderfalk@Alexanders-MacBook-Pro.local>
2017-10-03 16:45:54 +0200

    Autodeploy Test 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -8,8 +8,14 @@ public class HelloWorld {
 
     @GET
     @Produces(MediaType.TEXT_XML)
-    public String getMessage() {
+    public String sayXMLHello() {
         return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello Jersey";
     }
 
 }
