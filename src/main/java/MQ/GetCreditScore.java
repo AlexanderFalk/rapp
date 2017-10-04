52d6e29b45b02c572d78c2b9acf3ee3d22d01dfc
KIMB0 <Kimsen1992@gmail.com>
2017-10-04 13:43:46 +0200

    Added a new class Loan.

--- src/main/java/MQ/GetCreditScore.java
+++ src/main/java/MQ/GetCreditScore.java
@@ -2,6 +2,7 @@ package MQ;
 
 import MQ.CreditScore.CreditScoreService;
 import MQ.CreditScore.CreditScoreService_Service;
+import MQ.Model.Loan;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
@@ -11,18 +12,21 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.concurrent.TimeoutException;
 
+
 public class GetCreditScore {
     private final static String QUEUE_NAME = "loanqueue";
 
     public double userLoanAount;
-    public Date date = new Date();
     public String ssn;
+    public Integer csResult;
 
     public static void main(String[] args) throws IOException, TimeoutException {
+        Loan loan = new Loan();
+
         GetCreditScore cds = new GetCreditScore();
         cds.ssn = "010192-1581";
-        System.out.println(creditScore(cds.ssn));
-
+        cds.csResult = creditScore(cds.ssn);
+        System.out.println(cds.csResult);
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
