b65cc36798d50e299989a0e25bc8a5a064eb2f5f
Danny Nielsen <danny.nlsn0@gmail.com>
2017-10-11 18:25:05 +0200

    Changed GetCreditScore, so that it's basically a LoanRequest instead.

--- src/main/java/MQ/GetCreditScore.java
+++ src/main/java/MQ/GetCreditScore.java
@@ -30,19 +30,15 @@ import java.util.concurrent.TimeoutException;
 
 
 public class GetCreditScore {
-    private final static String QUEUE_NAME = "loan";
-    public byte[] xmlBytes;
+    private static final String HOST_NAME = "207.154.228.245";
+    private final static String PUBLISH_QUEUE_NAME = "Loan_Request_Queue";
 
     public static void main(String[] args) throws IOException, TimeoutException, SAXException, ParseException {
         Loan loan = new Loan();
-        GetCreditScore cds = new GetCreditScore();
 
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter your social security number in the format ******-****:");
         loan.setSSN(reader.next());
-
-        // Sets the credit score to loan.creditScore
-        loan.setCreditScore(creditScore(loan.getSSN()));
 
         System.out.println("Enter how much you want to loan:");
         loan.setLoanAmount(reader.nextDouble());
@@ -62,44 +58,32 @@ public class GetCreditScore {
         String finalLoanDuration = output + " 01:00:00.0 CET";
 
         // Runs the writeXML method
-        cds.writeXML(loan.getSSN(), loan.getCreditScore(), loan.getLoanAmount(), finalLoanDuration, loan.getInterestRate(), loan.getRules());
+        writeXML(loan.getSSN(), loan.getLoanAmount(), finalLoanDuration);
 
 
     }
 
     // The method that makes the XML file
-    public void writeXML(String ssnumber, int creditScore, double loanAmount, String loanDuration, double interestRate, String[] rules) {
+    private static void writeXML(String ssnumber, double loanAmount, String loanDuration) {
         try {
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
             Document doc = docBuilder.newDocument();
-            Element rootElement = doc.createElement("loan");
+            Element rootElement = doc.createElement("LoanRequest");
             doc.appendChild(rootElement);
 
             Element ssn = doc.createElement("ssn");
             ssn.appendChild(doc.createTextNode(ssnumber));
             rootElement.appendChild(ssn);
 
-            Element credScore = doc.createElement("creditscore");
-            credScore.appendChild(doc.createTextNode(String.valueOf(creditScore)));
-            rootElement.appendChild(credScore);
-
-            Element loanAm = doc.createElement("loanamount");
+            Element loanAm = doc.createElement("loanAmount");
             loanAm.appendChild(doc.createTextNode(String.valueOf(loanAmount)));
             rootElement.appendChild(loanAm);
 
-            Element loanDu = doc.createElement("loanduration");
+            Element loanDu = doc.createElement("loanDuration");
             loanDu.appendChild(doc.createTextNode(loanDuration));
             rootElement.appendChild(loanDu);
-
-            Element interestRa = doc.createElement("interestrate");
-            interestRa.appendChild(doc.createTextNode(String.valueOf(interestRate)));
-            rootElement.appendChild(interestRa);
-
-            Element ruls = doc.createElement("rules");
-            ruls.appendChild(doc.createTextNode(String.valueOf(rules)));
-            rootElement.appendChild(ruls);
 
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
@@ -109,13 +93,12 @@ public class GetCreditScore {
 
             transformer.transform(source, result);
 
-            byte[] array = bos.toByteArray();
-            xmlBytes = array;
+            byte[] xmlByteArray = bos.toByteArray();
 
             System.out.println("=== File converted to byte array ===");
 
             // When all the above is done, then sendMesseage() begin
-            sendMessage();
+            sendMessage(xmlByteArray);
 
 
         } catch (ParserConfigurationException e) {
@@ -133,26 +116,20 @@ public class GetCreditScore {
     }
 
     // The messaging method, which in the moment sends a test string
-    public void sendMessage() throws IOException, TimeoutException {
+    private static void sendMessage(byte[] message) throws IOException, TimeoutException {
 
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("207.154.228.245");
+        factory.setHost(HOST_NAME);
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        channel.basicPublish("", QUEUE_NAME, null, xmlBytes);
-        System.out.println(" [X] Sent '" + xmlBytes + "'");
+        channel.queueDeclare(PUBLISH_QUEUE_NAME, false, false, false, null);
+        channel.basicPublish("", PUBLISH_QUEUE_NAME, null, message);
+        System.out.println(" [X] Sent '" + message + "'");
 
         channel.close();
         connection.close();
     }
 
-
-    private static int creditScore(java.lang.String ssn) {
-        CreditScoreService_Service service = new CreditScoreService_Service();
-        CreditScoreService port = service.getCreditScoreServicePort();
-        return port.creditScore(ssn);
-    }
 }
 
